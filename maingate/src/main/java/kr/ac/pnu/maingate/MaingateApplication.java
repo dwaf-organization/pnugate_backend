dd4434cae2ce63b9eed420cd964e6823bd7fb44a
donguk585 <137376258+donguk585@users.noreply.github.com>
2026-03-09 13:03:00 +0900

    first update

---
+++ maingate/src/main/java/kr/ac/pnu/maingate/MaingateApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.pnu.maingate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaingateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaingateApplication.class, args);
+	}
+
+}
---
+++ maingate/src/test/java/kr/ac/pnu/maingate/MaingateApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.pnu.maingate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaingateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
